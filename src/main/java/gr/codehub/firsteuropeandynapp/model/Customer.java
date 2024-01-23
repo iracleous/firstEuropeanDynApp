7821f6e2b0481a8d3ff02f65672f9e5211aa69d5
dimitris <iracleous@gmail.com>
2024-01-23 10:07:20 +0200

    h2 db

--- src/main/java/gr/codehub/firsteuropeandynapp/model/Customer.java
+++ src/main/java/gr/codehub/firsteuropeandynapp/model/Customer.java
@@ -1,15 +1,21 @@
 package gr.codehub.firsteuropeandynapp.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.*;
 
 import java.sql.Date;
 
 @Data
 //avoiding boilerplate code
-
+@Entity
 //model, entity, domain,
 public class Customer {
-    private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     private String name;
     private String email;
     private Date registrationDate;
